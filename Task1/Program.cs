aaa4307ea5aa1a418804b52c7fc5c53afb33aff3
LexLuter <gae_cbr@mail.ru>
2023-03-03 19:29:40 +0700

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿int maxM = 8;
+Console.Write($"Введите {maxM} чисел: \n");
+int result = 0;
+int[] array = new int[maxM];
+for (int i = 0; i < maxM; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    if (array[i] > 0) result++;
+}
+Console.WriteLine($"Массив: [ {String.Join(", ", array)} ]");
+Console.WriteLine($"Пользователь  ввел {result} чисел больше 0.");
